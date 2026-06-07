4f2832995a9ae81fd839915e035745e4136f8192
mii3131 <miie0222@i.softbank.jp>
2026-06-07 22:53:25 +0900

    おみくじアプリ作成

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 'use strict';
 const http = require('node:http');
+const fs = require('node:fs');
 const server = http
   .createServer((req, res) => {
     const now = new Date();
     console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
+      'Content-Type': 'text/html; charset=utf-8'
     });
 
     switch (req.method) {
       case 'GET':
-        res.write(`GET ${req.url}\n`);
+        const rs = fs.createReadStream('./form.html');
+        rs.pipe(res);
         break;
-      case 'POST':
-        res.write(`POST ${req.url}\n`);
-        let rawData = '';
-        req
-          .on('data', chunk => {
-            rawData += chunk;
-          })
-          .on('end', () => {
-            console.info(`[${now}] Data posted: ${rawData}`);
-          });
-        break;
+case 'POST':
+  const result = Math.random() < 0.5 ? '当たり！🎉' : 'ハズレ…😢';
+
+  res.write(`
+    <!DOCTYPE html>
+    <html lang="ja">
+    <body>
+      <h1>${result}</h1>
+      <a href="/">もう一回引く</a>
+    </body>
+    </html>
+  `);
+
+  res.end();
+  break;
       default:
         break;
     }
-    res.end();
   })
   .on('error', e => {
     console.error(`[${new Date()}] Server Error`, e);
